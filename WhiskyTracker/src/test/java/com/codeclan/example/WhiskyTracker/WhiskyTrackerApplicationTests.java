24ff3edc4e3f0b2bce1144343f8f7b02dbe5a924
Olive-kaa <omkupinska@gmail.com>
2023-02-01 16:45:54 +0000

    uploading mvp

---
+++ WhiskyTracker/src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
@@ -0,0 +1,40 @@
+package com.codeclan.example.WhiskyTracker;
+
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test") //Indicates it's a test profile so will not run DataLoader
+@SpringBootTest
+public class WhiskyTrackerApplicationTests {
+
+	@Autowired
+	WhiskyRepository whiskyRepository;
+
+	@Autowired
+	DistilleryRepository distilleryRepository;
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void canFindWhiskyByDistilleryAndAge(){
+		List<Whisky> whiskies = whiskyRepository.findWhiskyByAgeAndDistilleryName(15, "Glendronach");
+//		assertEquals(1, whiskies.size());
+		assertEquals(15, whiskies.get(0).getAge());
+		assertEquals("Glendronach", whiskies.get(0).getDistillery().getName());
+	}
+
+}
